ac414b27519b54e9e6d442b9da63c802a186c5be
genaro gonzalez <genarrogg@gmail.com>
2024-06-02 17:19:29 -0400

    creamcion de carreras

--- src/config/InicializarDB/carreras.ts
+++ src/config/InicializarDB/carreras.ts
@@ -2,29 +2,9 @@ import { Carrera } from "../../models";
 import carreras from "./info/carreras";
 
 const populateCarreras = async () => {
+
   try {
-    const carrerasData = [
-      {
-        IngenieriaInformatica: {
-          nombre: "Ingeniería de Sistemas",
-          pensumCode: "IS01",
-          semestral: true,
-          anual: false,
-          estado: true,
-          creditosTotales: 200,
-          facultad: "Ingeniería",
-        },
-        IngenieriaCivil: {
-          nombre: "Ingeniería Civil",
-          pensumCode: "IC01",
-          semestral: true,
-          anual: false,
-          estado: true,
-          creditosTotales: 210,
-          facultad: "Ingeniería",
-        },
-      },
-    ];
+    const carrerasData = carreras;
 
     const carrerasToInsert = Object.values(carrerasData[0]);
 
